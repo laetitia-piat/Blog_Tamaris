9b82ad4fe80f9b95393a7f400291880b16566342
Laetitia <laeptitchat29@gmail.com>
2025-05-21 14:30:26 +0200

    new dist

--- backend/dist/entities/User.js
+++ backend/dist/entities/User.js
@@ -58,8 +58,7 @@ __decorate([
     __metadata("design:type", String)
 ], UserInput.prototype, "password", void 0);
 __decorate([
-    (0, type_graphql_1.Field)(),
-    (0, typeorm_1.Column)({ nullable: true }),
+    (0, type_graphql_1.Field)({ nullable: true }),
     __metadata("design:type", Number)
 ], UserInput.prototype, "residentId", void 0);
 __decorate([
