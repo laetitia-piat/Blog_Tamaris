ae40546023d890a5e13a61a99636df43998d8e6e
Laetitia <laeptitchat29@gmail.com>
2025-05-21 11:39:38 +0200

    ajout d'un background

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,14 +1,42 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import PostCard from "../components/PostCard";
 import {
+  LoginUserInput,
   useGetAllPostsQuery,
   useGetUserInfoQuery,
+  useLoginMutation,
 } from "../generated/graphql-types";
+import { GET_USER_INFOS } from "../graphql/queries";
+import { SubmitHandler, useForm } from "react-hook-form";
 
 const HomePage = () => {
   const { loading, error, data } = useGetAllPostsQuery();
   const userInfos = useGetUserInfoQuery();
   console.log(userInfos.data);
+
+  const navigate = useNavigate();
+  const [login] = useLoginMutation({
+    refetchQueries: [{ query: GET_USER_INFOS }],
+  });
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginUserInput>();
+  const onSubmit: SubmitHandler<LoginUserInput> = (data) => {
+    console.log(data);
+    login({
+      variables: { data: { email: data.email, password: data.password } },
+      onCompleted: (result) => {
+        localStorage.setItem("token", result.login);
+        navigate("/");
+      },
+      onError: (error) => {
+        console.log(error);
+      },
+    });
+  };
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Error!</p>;
   if (data && userInfos.data?.getUserInfo.isLoggedIn) {
@@ -44,14 +72,40 @@ const HomePage = () => {
           backgroundImage: "url('/images/tamaris_background.jpg')",
         }}
       >
-        <h1 className="text-6xl text-[#3c5c39] font-bold">
-          Bienvenu sur le blog des Tamaris
+        <h1 className="text-6xl text-[#3c5c39] font-bold pt-40">
+          BLOG DES TAMARIS
         </h1>
-        {/* <Link to="/login">
-          <button className="bg-[#4c7d48] p-3 w-32 rounded-full text-white text-2xl mt-20">
-            Login
-          </button>
-        </Link> */}
+        <form
+          className="flex flex-col border rounded-lg w-1/2 m-auto mt-50 pt-10"
+          onSubmit={handleSubmit(onSubmit)}
+        >
+          <input
+            className="mb-10 w-1/2 m-auto"
+            placeholder="email"
+            {...register("email", { required: true })}
+          />
+          {errors.password && <span>This field is required</span>}
+
+          <input
+            className="mb-10 w-1/2 m-auto"
+            placeholder="password"
+            type="password"
+            {...register("password", { required: true })}
+          />
+
+          {errors.password && <span>This field is required</span>}
+          <input
+            className="bg-[#4c7d48] p-2 w-32 m-auto rounded-full text-white"
+            type="submit"
+          ></input>
+
+          <a className="mt-5 text-center" href="register">
+            Pas encore inscrit?
+          </a>
+          {/* <a className="linkSubscribe" href="forgotPassword">
+            Mot de passe oublié?
+          </a> */}
+        </form>
       </div>
     );
   }
