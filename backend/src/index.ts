11dbf09b6c70264feafd823991f7c2228b473a3a
Laetitia <laeptitchat29@gmail.com>
2025-07-02 13:52:44 +0200

    userole modifié par role pour context

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -49,7 +49,7 @@ const start = async () => {
             console.log("payload was found and returned to resolver");
             return {
               userName: payload.userName,
-              userRole: payload.userRole,
+              role: payload.role,
               res: res,
             };
           }
