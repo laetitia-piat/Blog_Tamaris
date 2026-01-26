267503e578282949afe08bf4c9e1c9ce2aea31ba
Laetitia <laeptitchat29@gmail.com>
2026-01-26 18:11:00 +0100

    import / export server

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,60 +2,24 @@ import "dotenv/config";
 import { ApolloServer } from "@apollo/server";
 import { startStandaloneServer } from "@apollo/server/standalone";
 import { dataSource } from "./config/db";
-import * as cookie from "cookie";
-import { buildSchema } from "type-graphql";
-import jwt, { Secret } from "jsonwebtoken";
-import { resolvers } from "./resolvers/Index";
+import { createContext } from "./auth/context";
+import { createSchema } from "./auth/schema";
 
 const start = async () => {
-  if (
-    process.env.JWT_SECRET_KEY === null ||
-    process.env.JWT_SECRET_KEY === undefined
-  ) {
-    throw Error("no secret key!");
-  }
+  if (!process.env.JWT_SECRET_KEY) throw Error("no secret key!");
+
   await dataSource.initialize();
 
-  const schema = await buildSchema({
-    resolvers,
-    emitSchemaFile: true,
-    authChecker: ({ context }) => {
-      if (context.userName) {
-        return true;
-      } else {
-        return false;
-      }
-    },
-  });
+  const schema = await createSchema();
 
-  const server = new ApolloServer({
-    schema,
-  });
+  const server = new ApolloServer({ schema });
+
   const { url } = await startStandaloneServer(server, {
     listen: { port: 4000 },
-    context: async ({ req, res }) => {
-      if (req.headers.cookie) {
-        const cookies: any = cookie.parse(req.headers.cookie as string);
-        if (cookies.token !== undefined) {
-          const payload: any = jwt.verify(
-            cookies.token,
-            process.env.JWT_SECRET_KEY as Secret,
-          );
-          console.log("payload in context", payload);
-          if (payload) {
-            console.log("payload was found and returned to resolver");
-            return {
-              userName: payload.userName,
-              role: payload.role,
-              res: res,
-            };
-          }
-        }
-      }
-      return { res: res };
-    },
+    context: createContext,
   });
 
   console.log(`🚀 Server listening at: ${url}`);
 };
+
 start();
