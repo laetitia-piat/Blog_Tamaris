1b8862b521ea82cd07fd43b58fe260bd0ce5cab4
Laetitia <laeptitchat29@gmail.com>
2026-01-26 18:47:04 +0100

    case

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,7 +5,7 @@ import { ApolloProvider } from "@apollo/client";
 
 import App from "./App.tsx";
 import "./index.css";
-import { apolloClient } from "./apollo/client.ts";
+import { apolloClient } from "./apollo/Client.ts";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
